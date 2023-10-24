8b116931ea59d5746220b8112db1973f44e42180
jacoblurie29 <jacoblurie29@gmail.com>
2023-10-23 20:49:07 -0500

    Added content to site

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
 import styles from "./App.module.css";
 
-function App() {
-  return <div className={styles.mainContainer}>Test</div>;
-}
+const App = () => (
+  <div className={styles.mainContainer}>
+    <h1 className={styles.header}>Welcome to our site!</h1>
+    <div className={styles.cardContainer}>
+      <div className={styles.card} style={{ backgroundColor: "#BBBBBB" }}>
+        Card 1
+      </div>
+      <div className={styles.card} style={{ backgroundColor: "#BBBBBB" }}>
+        Card 2
+      </div>
+      <div className={styles.card} style={{ backgroundColor: "#BBBBBB" }}>
+        Card 3
+      </div>
+      <div className={styles.card} style={{ backgroundColor: "#BBBBBB" }}>
+        Card 4
+      </div>
+      <div className={styles.card} style={{ backgroundColor: "#BBBBBB" }}>
+        Card 5
+      </div>
+      <div className={styles.card} style={{ backgroundColor: "#BBBBBB" }}>
+        Card 6
+      </div>
+      <div className={styles.card} style={{ backgroundColor: "#BBBBBB" }}>
+        Card 7
+      </div>
+      <div className={styles.card} style={{ backgroundColor: "#BBBBBB" }}>
+        Card 8
+      </div>
+      <div className={styles.card} style={{ backgroundColor: "#BBBBBB" }}>
+        Card 9
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
