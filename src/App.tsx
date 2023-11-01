93b854c08c3626c5d4bf63cb819939fdcc47eb4b
Neelasha Bhattacharjee <neelashab@gmail.com>
2023-11-01 13:54:00 -0500

    Neelasha Workshop 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,58 @@
+import { useEffect } from "react";
 import styles from "./App.module.css";
+import {faker} from "@faker-js/faker";
 
-const App = () => (
+interface Flight {
+  flightNumber: string;
+  origin: string;
+  destination: string;
+  departureTime: Date;
+  arrivalTime: Date;
+  flightDuration: number;
+}
+
+
+
+const App = () => {
+
+  const [allFlights, setFlights] = useState(0);
+  
+
+  useEffect(() => {
+
+  })
+
+
+
+  function generateFlights(): Flight{
+
+    const flight: Flight = {
+      flightNumber: faker.string.alphanumeric(6),
+      origin: faker.string.alphanumeric(3),
+      destination: faker.string.alphanumeric(3),
+      departureTime: faker.date.anytime(),
+      arrivalTime: faker.date.anytime(),
+      flightDuration: faker.number.int({min: 1, max: 8})
+    }
+
+    return flight;
+  } 
+
+
+
+
+
+  return (
   <div className={styles.mainContainer}>
-    <h1 className={styles.header}>Welcome to our site!</h1>
+    <div className = "genButton">
+      <button type="button" onClick={generateFlights}>
+        Generate a New Flight
+      </button>
+
+    </div>
+    
   </div>
-);
+  )
+};
 
 export default App;
